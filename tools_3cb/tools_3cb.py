f69c006abe36e1877a41382388ab54f9b90bb767
Leo Ghignone <leo.ghignone@utas.edu.au>
2024-12-20 22:26:17 +1100

    Improve estimated score, add card suggestions

--- tools_3cb/tools_3cb.py
+++ tools_3cb/tools_3cb.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import pandas as pd
 
 logging.basicConfig(level=logging.INFO)
-
+logger = logging.getLogger()
 
 class Tools3CB:
     def __init__(self, banlist="banlist.txt", database="database"):
@@ -16,7 +16,11 @@ class Tools3CB:
         """
         Load the results of a deck from the database
         """
-        return pd.read_csv(f"{self.database}/{deck}.csv", index_col=0)
+        try:
+            results = pd.read_csv(f"{self.database}/{deck}.csv", index_col=0)
+        except FileNotFoundError:
+            results = pd.DataFrame(columns=["Result"])
+        return results
 
     def save_deck(self, deck):
         """
@@ -78,7 +82,7 @@ class Tools3CB:
                 deck_db = deck_db.drop(deck)
         return deck_db
 
-    def get_suggestions(self, gauntlet, threshold=None):
+    def get_suggestions(self, gauntlet, threshold=None, estimate=True):
         """
         Returns a DataFrame with all decks that have a known score above the threshold
         against the gauntlet
@@ -86,17 +90,51 @@ class Tools3CB:
         table = pd.DataFrame()
         for deck in gauntlet:
             if deck not in self.decklist:
-                raise ValueError(f"Deck {deck} not found in database")
+                logger.warning(f"Deck {deck} not found in database")
+                table[deck] = pd.Series(dtype="float")
+                continue
             # Negate values as we are loading the reverse results
             deck_db = -self.load_deck(deck)
             deck_db = self.remove_banlist(deck_db)
             deck_db = deck_db.rename(columns={"Result": deck})
             table = pd.concat([table, deck_db], axis=1)
+        result = table.copy()
+        result.insert(0, "Known score", table.sum(axis=1))
+        if threshold is not None:
+            result = result[result["Known score"] > threshold]
+        if estimate:
+            est = self.fill_guesses(table)
+            result.insert(1, "Estimated score", est.sum(axis=1))
+            result = result.sort_values(by="Estimated score", ascending=False)
+        result = result.sort_values(by="Known score", ascending=False, kind="stable")
+        result.index.name = "Suggested Deck"
+        return result
+
+    def get_card_suggestions(self, gauntlet, threshold=None, remove_banlist=True):
+        """
+        Returns a DataFrame with all cards that have a known score above the threshold
+        against the gauntlet
+        """
+        table = pd.DataFrame()
+        for deck in gauntlet:
+            if deck not in self.decklist:
+                logger.warning(f"Deck {deck} not found in database")
+                table[deck] = pd.Series(dtype="float")
+                continue
+            # Negate values as we are loading the reverse results
+            deck_db = -self.load_deck(deck)
+            if remove_banlist:
+                deck_db = self.remove_banlist(deck_db)
+            # deck_db.index is a series of opponent decks. Split each on ' | ' to obtain a list of cards
+            cards_db = deck_db.assign(cards=deck_db.index.str.split(' | ', regex=False))
+            cards_db = cards_db.explode("cards").groupby("cards").mean()
+            cards_db = cards_db.rename(columns={"Result": deck})
+            table = pd.concat([table, cards_db], axis=1)
         table.insert(0, "Total", table.sum(axis=1))
         if threshold is not None:
             table = table[table["Total"] > threshold]
         table = table.sort_values(by="Total", ascending=False)
-        table.index.name = "Suggested Deck"
+        table.index.name = "Suggested Card"
         return table
 
     def guess_result(self, deck, opponent):
@@ -120,7 +158,8 @@ class Tools3CB:
                     opp_cards.remove(card)
                     similarity += 1
             guess = deck_db.loc[opp, "Result"]
-            for _ in range(similarity):
+            # for _ in range(similarity):
+            if similarity>1:
                 guesses.append(guess)
         return guesses
 
@@ -129,10 +168,6 @@ class Tools3CB:
             for opponent in table.columns:
                 if pd.isna(table.loc[deck, opponent]):
                     table.loc[deck, opponent] = self.guess_result(deck, opponent)
-        # Recompute totals
-        table = table.drop("Total", axis=1)
-        table.insert(0, "Total", table.sum(axis=1))
-        table = table.sort_values(by="Total", ascending=False)
         return table
 
 
@@ -142,6 +177,5 @@ if __name__ == "__main__":
     table = tools.get_suggestions(gauntlet, 0)
     print(table)
     table.to_csv("suggestions.csv")
-    table = tools.fill_guesses(table)
-    print(table)
-    table.to_csv("suggestions_guesses.csv")
+    cards = tools.get_card_suggestions(gauntlet)
+    print(cards)
